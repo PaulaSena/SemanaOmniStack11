cc35fc8549f90c0cafda1532d697d9f17b83da5f
Paula Sena <senasilvaanapaula@gmail.com>
2020-04-24 18:53:38 -0300

    Feita a instalação global do npm i -g create-react-app para iniciar o projeto com npx para ReactJs

--- backEnd/index.js
+++ backEnd/index.js
@@ -13,11 +13,31 @@ app.get('/', (req, res) => {
 
 */
 
-// Rota execultada com sucesso !!
-app.get('/', (req, res) => res.send('Hello World!'))
+// Rota execultada com sucesso !!  send fomato texto
 
+/*app.get('/', (req, res) => res.send('Hello World!'))
+*/
+
+// Reposta no formato Json, EXECULTADO COM SUCESSO objeto em javaScript ex:dados de usuario.
+
+/*app.get('/', (req, res) => res.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Ana Paula'
+})
+)*/
+
+// Retornando uma reposta no formato Json, EXECULTADO COM SUCESSO objeto em javaScript ex:dados de usuario.
+
+app.get('/', (req, res) => {
+    return res.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Ana Paula'
+})
+})
 
 // Definindo porta de localhost:
 app.listen(3333);
 
 //Teminal executar aplicação | node index.js
+
+// Feita a instalação do npm i -g create-react-app para iniciar o projeto com ReactJs 
