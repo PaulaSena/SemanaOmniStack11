99101346fda26669c799dd622b9a6b157fd742d7
Paula Sena <senasilvaanapaula@gmail.com>
2020-06-20 20:13:17 -0300

    BackEnd da aplicação

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,58 +1,99 @@
-// Terminal pagina principal execultado o |npm install express | ao instalar aparecerá as dependências
-// O Framworks criou automaticamente as pastas com os códigos fontes das dependências instaladas do próprio express.  
-// Criado arquivo index.js 
+/**** Passo anterior.
+ *  Terminal pagina principal execultar:
+| ~> npm install express 
+*** O Framworks cria automaticamente as pastas com os códigos fontes e dependências do próprio express.  
+*/
 
-// Base | importando funcionalidades do express para variavel.
+// 1 - Criado esse arquivo index.js dentro da pasta backEnd.
+
+
+// 2 - Base | importando funcionalidades do express para variavel.
 const express = require('express');
 
-// Criando variavel para aplicação
+// 3 - Criando variavel para aplicação
 const app = express();
 
-// Traçando rota
+// Quando Undefined Informar ao navegador que requisições pode vim em Json 
+app.use(express.json());
+
+// 4 - Traçando rota.
+
+// Rota é um conjunto com *Método  *Recurso, *Requisição e Resposta.
+
+/**** Rota (app.get) -> get método de busca.  
+* ('/') *Recurso -> Local especifico para ser acessado 
+*  Ex: Listar USERS no banco http://localhost:3333/ USERS
+*/
 
 
-/*   
-Falha na execução
+
+/**** Resposta execultada no formato de texto Send  ex1
 app.get('/', (req, res) => {
     return res.send('Hello World');
 }); 
 */
 
 
-// Rota execultada com sucesso !!  send fomato texto
-//app.get('/', (req, res) => res.send('Hello World!'))
-
-
-/*
-Reposta no formato Json, EXECULTADO COM SUCESSO objeto em javaScript ex:dados de usuario.
-
-app.get('/', (req, res) => res.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Ana Paula'
-})
-)
-*/
-
-// Retornando uma reposta no formato Json, EXECULTADO COM SUCESSO objeto em javaScript ex:dados de usuario.
-
-/***Rota (app.get) ->conjunto completo   ('/') Recurso -> Local especifico para ser acessado - Ex: Listar usuários no banco
-http://localhost:3333/users
+/**** Resposta execultada no formato de texto Send  ex2
+app.get('/', (req, res) => res.send('Hello World!'))
 */
 
 
-app.get('/users', (req, res) => {
+
+/*** Retornando uma reposta no formato Json = (objeto em javaScript) ex:dados de usuario.
+app.get('/', (req, res) => {
     return res.json({
     evento: 'Semana OmniStack 11.0',
-    aluno: 'Ana Paula'
+    aluno: 'Ana Paula Sena'
+})
+})
+*/
+
+//*** Criando um usuario com método POST e realizando teste com software Postman!
+/*
+app.get('/users/', (req, res) => {
+    return res.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Ana Paula Silva'
+})
+})
+*/
+
+/* (Criar Editar)  Metodo Post  para criar Usuario
+*  Request  Body : Parâmetros para criar ou alterar recurso
+*  EX: Nome E-mail senha.
+*/
+ 
+    app.post('/users', (req, res) => {
+        const body=req.body;
+
+        console.log(body);
+
+    return res.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Theo David'
 })
 })
 
-/****Métodos 
+
+
+
+
+/**Métodos HTTP
 * HTTP Get ->Buscar uma informação do back-end - Ex. Buscar informação de Usuário
 * HTTP Post ->Criar uma informação no back-end - Ex. Cria Usuário.
 * HTTP Put -> Alterar uma informação no back-and.
 * HTTP Delete: Deletar uma informação no back-end.
-*/
+**/
+
+/** Tipos de Parametros
+ * Query Parms -> Parâmetros nomeados enviados na rota após“?” GET *Ex (filtros, paginação)        
+ * Route Parms -> Parâmetros para identificar recursos. Get *Ex. ID usuário especifico.
+ * Request  Body -> Corpo da requisição do usuário.
+ * HTTP Delete -> Deletar uma informação no back-end.
+
+
+ **/
 
 // Definindo porta de localhost:
 app.listen(3333);
@@ -60,18 +101,22 @@ app.listen(3333);
 
 
 
-/* Pasta principal realizada a instalação do npm i -g create-react-app para iniciar o projeto com ReactJs 
-                       | npx create-react-app frontend
-Feito a instalação dele, aí você poderá utilizar o mesmo para criar novos projetos.
- */
+/****  No terminal Pasta principal realizar a instalação do app react. 
+|~> npm i -g create-react-app 
+*** Para iniciar o projeto com ReactJs digite o comando seguinte... e por ultimo o nome da pasta.
+|~> npx create-react-app frontend
+***Feito a instalação dele, poderá utiliza-lo para criar novos projetos.
+ ****/
 
-//Teminal executar na pasta frontEnd o comando | npm start | -> esse comando iniciara um script dentro do package.json
+/**** Executar Teminal na pasta frontEnd:
+| ~> npm start 
+// esse comando iniciara um script dentro do package.json
 
 
 
-// Teminal executar aplicação | node index.js
+// Executar Teminal na rota da aplicação 
+|~> node index.js
 
-/** Start realizado | React app  
- *Edição realizada na pasta src/App.js do arquivo carregado 
- no React app  http://localhost:3000 
+/** Start realizado =) | React app  
+ *Edição realizada =) Ola mundo, pasta src/App.js no React app  http://localhost:3000 
 **/
